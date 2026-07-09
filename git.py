b705fba368dd702f9834411e85f7743181dc00ed
Aayush <aayushkumarthe@gmail.com>
2026-07-09 21:49:59 +0530

    Updated

--- git.py
+++ git.py
@@ -1,8 +1,13 @@
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+d = int(input("Enter fourth number: "))
 
 a, b = b, a
+c, d = d, c
 
 print("After swapping:")
 print("a =", a)
 print("b =", b)
+print("c =", c)
+print("d =", d)
