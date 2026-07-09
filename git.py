aba93a1b7dd3472803da06702a3668e37297ddea
Aayush <aayushkumarthe@gmail.com>
2026-07-09 18:34:53 +0530

    done

--- git.py
+++ git.py
@@ -1,6 +1,8 @@
-num = int(input("Enter a number: "))
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
 
-if num % 2 == 0:
-    print("Even")
-else:
-    print("Odd")
+a, b = b, a
+
+print("After swapping:")
+print("a =", a)
+print("b =", b)
